61f12725f78cad10d890c5dcf8989a8d2d6a519a
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-01-29 21:38:40 -0200

    User Migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ Message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Israel Bonetti',
+    email: 'israelbonetti@gmail.com',
+    password_hash: 'asld11',
+  });
+  res.json(user);
 });
 
 export default routes;
