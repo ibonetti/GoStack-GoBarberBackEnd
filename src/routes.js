1b273534b0273f22b9c12daa9538864586e4e1ce
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-01-29 01:03:16 -0200

    creating configs

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  res.json({Message: "Hello World"})
-})
+routes.get('/', (req, res) => {
+  res.json({ Message: 'Hello World' });
+});
 
 export default routes;
