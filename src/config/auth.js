384230a4933c994618f2eca926f50a14e22969af
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-01-30 20:10:32 -0200

    Jwt authentication

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'de68d04475440be40cf8b415ba92ea11',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Israel Bonetti',
-    email: 'israelbonetti@gmail.com',
-    password_hash: 'asld11',
-  });
-  res.json(user);
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;
