6c7a467917165e8eaaa6041eebd5c5dfecb65fe7
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-02-02 22:38:50 -0200

    Schedule Controller

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-
+import ScheduleController from './app/controllers/ScheduleController';
 import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,11 +20,13 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), FileController.store);
-
 routes.get('/providers', ProviderController.index);
 
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
