41d7a71ef6742e67de69f4a9a3612f3c01b291fe
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-02-03 22:20:45 -0200

    Emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7d724f5166e30e',
+    pass: 'c4d400156ff82c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
