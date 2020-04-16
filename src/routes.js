de259728b5e05dd911ac63488ff978b67b65d6b4
Israel Bonetti <israelbonetti@MacBook-Pro-de-Israel.local>
2020-04-16 20:34:51 -0300

    Fixes

--- src/routes.js
+++ src/routes.js
@@ -18,6 +18,7 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.get('/users', UserController.index);
 routes.post('/sessions', SessionController.store);
+routes.get('/', (req, res) => res.send('ok'));
 
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
